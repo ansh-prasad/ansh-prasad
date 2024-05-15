0db3afaa0134c963455a3e841ddd05cb0c56a881
Ansh Prasad <129548078+ansh-prasad@users.noreply.github.com>
2024-05-15 19:34:03 +0530

    Update change_theme.rb

--- change_theme.rb
+++ change_theme.rb
@@ -1,1 +1,6 @@
-https://leetcode-stats-six.vercel.app/?username=anshprasad01&theme=tokyonight
+# change_theme.rb
+theme = "tokyonight"
+username = "anshprasad01"
+markdown_code = "[![KnlnKS's LeetCode stats](https://leetcode-stats-six.vercel.app/?username=#{username}&theme=#{theme})](https://github.com/#{username}/leetcode-stats)"
+
+puts markdown_code
