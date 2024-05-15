b2c6cf43cb83694c81e7ee7560f78a79137258bc
Ansh Prasad <129548078+ansh-prasad@users.noreply.github.com>
2024-05-15 19:35:55 +0530

    Delete change_theme.rb

--- change_theme.rb
+++
@@ -1,6 +0,0 @@
-# change_theme.rb
-theme = "tokyonight"
-username = "anshprasad01"
-markdown_code = "[![KnlnKS's LeetCode stats](https://leetcode-stats-six.vercel.app/?username=#{username}&theme=#{theme})](https://github.com/#{username}/leetcode-stats)"
-
-puts markdown_code
